a50917f87e254bbde8ceca075787e941454faf49
Karl Söderberg <karl.soderberg@shortcut.io>
2022-06-29 23:11:12 +0200

    Initial Commit

---
+++ cube-track/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  cube-track
+//
+//  Created by Karl Söderberg on 2022-06-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cube-track/cube_trackApp.swift
@@ -0,0 +1,17 @@
+//
+//  cube_trackApp.swift
+//  cube-track
+//
+//  Created by Karl Söderberg on 2022-06-29.
+//
+
+import SwiftUI
+
+@main
+struct cube_trackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
